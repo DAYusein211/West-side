c5aa2382ecaa12ce4f16de840a85f2c6e956818f
DAYusein211 <DAYusein21@codingburgas.bg>
2024-01-03 17:25:28 +0200

    Fix coloring

--- website/components/signIn.jsx
+++ website/components/signIn.jsx
@@ -5,9 +5,9 @@ import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 export default function SignIn()
 {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [error, setError] = useState("")
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
     const router = useRouter();
     
@@ -18,11 +18,12 @@ export default function SignIn()
            const res = await signIn("credentials", 
            {
             email, password, redirect:false});
+
            if(res.error)
            {
                setError("Invalid password or username") 
                 return;
-            }
+            }   
            router.replace("data")
         }
         
@@ -31,21 +32,21 @@ export default function SignIn()
         }
     }
     return (
-    <div className="grid place-items-center h-screen">
-        <div className = "shadow-md p-3 rounded-r-sm ">
-            <h1 className = "text-xl">
-                Email/username
+    <div className="flex place-items-center h-screen  justify-center items-center flex-col" >
+        <div className="grid place-items-center ">
+            <h1 className = "myFont spacing text-white text-xl align-middle font-serif tracking-wider font-thin">
+                Sign in to continue
             </h1>
-            <form className = "flex flex-col gap-2" onSubmit={HandleSubmit}>
-                <input type="text" onChange = {(e) => setEmail(e.target.value)} placeholder="Enter email or username"/>
-                <input type="text" onChange = {(e) => setPassword(e.target.value)} placeholder="Enter email or username"/>
-                <button className = "bg-blue-400 text-white">Sign in</button>
-                {error && <div className = "text-red-400">{error}</div>}
+            <form className = "flex place-items-center flex-col gap-2" onSubmit={HandleSubmit}>
+                <input type="text" onChange = {(e) => setEmail(e.target.value)} placeholder="Email"/>
+                <input type="text" onChange = {(e) => setPassword(e.target.value)} placeholder="Password"/>
+                <button className = "bg-blue-400 text-white w-32 self-center">Sign in</button>
+                {error && <div className = "text-red-400 ">{error}</div>}
                 
                 
-            <Link href = {"signUp"} className = "underline"> Sign Up</Link>
+            <Link href = {"signUp"} className = "underline text-white"> Sign Up</Link>
             </form>
-        </div>
+            </div>
     </div>
         );
 }
