454f408c3383517ff3a70cf3aed2bccbf2af963d
DAYusein211 <DAYusein21@codingburgas.bg>
2024-01-08 21:16:43 +0200

    Create custom cursor (close #6)

--- website/app/layout.js
+++ website/app/layout.js
@@ -3,7 +3,6 @@
 import './globals.css'
 import { AuthProvider } from './providers'
 import Link from 'next/link'
-
 import { useEffect } from 'react';
 import gsap from 'gsap';
 
@@ -14,40 +13,63 @@ export default function RootLayout({ children })
     const handleMove = (e) => 
     {
       const { clientX, clientY } = e;
-      const xOffset = -((clientX / window.innerWidth - 0.1) * 30);
-      const yOffset = -((clientY / window.innerHeight - 0.1) * 30);
+      
+      const xOffset = -((clientX / window.innerWidth - 0.1) * 15) * 0.95;
+      const yOffset = -((clientY / window.innerHeight - 0.1) * 15) * 0.95;
 
       gsap.to('.move1', 
       {
-        x: xOffset * 0.9,
-        y: yOffset * 0.9,
+        x: xOffset,
+        y: yOffset,
         duration:1.5,
-        ease:"power2.out"
+        ease:"power1.out"
       });
       gsap.to('.move2', 
       {
-        x: xOffset * 0.8,
-        y: yOffset * 0.8,
-        duration:1.5, 
-        ease:"power2.out"
+        x: xOffset,
+        y: yOffset,
+        duration:2, 
+        ease:"power1.out"
       });
       gsap.to('.move3', 
       {
-        x: xOffset * 0.7,
-        y: yOffset * 0.7,
-        duration:1.5, 
-        ease:"power2.out"
+        x: xOffset,
+        y: yOffset,
+        duration:2.5, 
+        ease:"power1.out"
       });
     };
 
     document.addEventListener('mousemove', handleMove);
+    const cursor = document.querySelector(".cursor");
+    
+  const updateCursor = (e) =>
+  {
+    cursor.style.top = e.clientY + "px";
+    cursor.style.left = e.clientX + "px";
+    console.log(e.clientY)
+  }
+  window.addEventListener("mousemove", updateCursor);
+  const link = document.querySelectorAll(".link");
+  
+    link.forEach(link => 
+      {
+          link.addEventListener("mouseover", () => {cursor.classList.add("scale")})
+          link.addEventListener("mouseleave", () => {cursor.classList.remove("scale")})
+      });
 
+  
     
   }, []);
 
+  
+
+    
   return (
     <html lang="en">
-      <body className= "w-screen h-screen"> <nav className=" w-[200px] sm:w-[400px] h-[30px] sm:h-[50px] flex flex-row gap-6 justify-end absolute right-0 mr-8">
+      <body className= "w-screen h-screen">
+        <div className = "cursor"></div>
+         <nav className=" w-[200px] sm:w-[400px] h-[30px] sm:h-[50px] flex flex-row gap-6 justify-end absolute right-0 mr-8">
         <Link href={"/wallet"}  className = "link move1">Wallet</Link>
         <Link href={"/transactions"} className = "link move2">Transactions</Link>
         <Link href={"/insurance"} className = "link move3" >Insurance</Link>
